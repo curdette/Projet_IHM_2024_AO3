b94415e6a7b47cd6525bfe95d5de9d3cfd2e8291
Cybiaé Cornet <150437994+curdette@users.noreply.github.com>
2024-11-24 16:29:36 +0100

    Update module-info.java

--- Projet_IHM_2024_AO3/src/module-info.java
+++ Projet_IHM_2024_AO3/src/module-info.java
@@ -1,5 +1,7 @@
-module Projet_IHM_2024_AO3 {
-	requires javafx.controls;
-	
-	opens application to javafx.graphics, javafx.fxml;
+module Jsouptest {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.jsoup; // assure-toi que Jsoup est bien ajouté au projet
+
+    opens application to javafx.graphics, javafx.fxml; // ouvre le package pour JavaFX
 }
